45e0986782126437b03d5f820c8cf94452b22238
ksngh <chzhqk98@naver.com>
2023-11-28 20:45:53 +0900

    test : 통합테스트 구축

--- src/main/java/practice/hellospring/SpringConfig.java
+++ src/main/java/practice/hellospring/SpringConfig.java
@@ -1,21 +1,26 @@
 package practice.hellospring;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import practice.hellospring.repository.JdbcMemberRepository;
 import practice.hellospring.repository.MemberRepository;
 import practice.hellospring.repository.MemoryMemberRepository;
 import practice.hellospring.service.MemberService;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
-
+    private final DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
